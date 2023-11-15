e1850b37a620a0615817be745c0df92d5f1559b9
Filipe Dias <44031127+jfilipedias@users.noreply.github.com>
2023-11-15 12:54:04 -0300

    feat: Start to implement base routes

--- README.md
+++ README.md
@@ -34,9 +34,9 @@ npm run test
 
 ### Functional Requirements
 
-- [ ] User must be able to register a meal (name, description, date and hour, if it is part of the diet);
-- [ ] User must be able to update a meal;
+- [x] User must be able to register a meal (name, description, date and hour, if it is part of the diet);
+- [x] User must be able to update a meal;
 - [ ] User must be able to delete a meal;
-- [ ] User must be able to list all meals;
-- [ ] User must be able to list a single meal;
+- [x] User must be able to list all meals;
+- [x] User must be able to list a single meal;
 - [ ] User must be able to retrieve a users metrics (total of registered meals, total of meals in the diet, total of non-dietary meals, best sequence of meals within the diet);
--- src/routes/meals.ts
+++ src/routes/meals.ts
@@ -1,5 +1,80 @@
+import { randomUUID } from 'node:crypto'
 import { FastifyInstance } from 'fastify'
+import { z } from 'zod'
+import { knex } from '@/database'
 
 export async function mealRoutes(app: FastifyInstance) {
-	app.post('/', async () => {})
+	app.post('/', async (request, reply) => {
+		const createMealBodySchema = z.object({
+			name: z.string(),
+			description: z.string(),
+			date: z.string(),
+			hour: z.string(),
+			diet: z.boolean(),
+		})
+
+		const { name, description, date, hour, diet } = createMealBodySchema.parse(
+			request.body,
+		)
+
+		await knex('meals').insert({
+			id: randomUUID(),
+			name,
+			description,
+			date,
+			hour,
+			diet,
+		})
+
+		return reply.status(201).send()
+	})
+
+	app.get('/', async () => {
+		const meals = await knex('meals').select('*')
+
+		return { meals }
+	})
+
+	app.get('/:id', async (request) => {
+		const getMealByIdParamsSchema = z.object({
+			id: z.string(),
+		})
+
+		const { id } = getMealByIdParamsSchema.parse(request.params)
+
+		const meal = await knex('meals').where({ id }).select('*').first()
+
+		return { meal }
+	})
+
+	app.put('/:id', async (request, reply) => {
+		const updateMealParamsSchema = z.object({
+			id: z.string(),
+		})
+
+		const { id } = updateMealParamsSchema.parse(request.params)
+
+		const updateMealBodySchema = z.object({
+			name: z.string(),
+			description: z.string(),
+			date: z.string(),
+			hour: z.string(),
+			diet: z.boolean(),
+		})
+
+		const { name, description, date, hour, diet } = updateMealBodySchema.parse(
+			request.body,
+		)
+
+		await knex('meals').where({ id }).update({
+			id,
+			name,
+			description,
+			date,
+			hour,
+			diet,
+		})
+
+		return reply.status(204).send()
+	})
 }
